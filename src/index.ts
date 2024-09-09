3f5fa1db371df4440cb67d431170b2822b295e7c
Anirudh Kalia <anikal2001@gmail.com>
2024-09-09 02:23:13 -0400

    Updated routers

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import "reflect-metadata";
 import { AppDataSource } from "./database/config";
 import { Router as userRouetr } from "./routers/user.routes";
-import { Router as carRouter } from "./routers/car.routes";
 
 dotenv.config();
 const app = express();
@@ -11,7 +10,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/api", userRouetr);
-app.use("/api", carRouter);
 AppDataSource.initialize()
   .then(() => {
     console.log("Database connected");
